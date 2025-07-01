5b7514937b77498f17b65dbd4e4bd562590dc109
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-07-01 16:04:51 +0200

    changing transaction type

--- src/app/api/actions/route.ts
+++ src/app/api/actions/route.ts
@@ -217,7 +217,7 @@ import {
      }).toString("base64");
 
            const responseBody: ActionPostResponse = {
-          type: "transaction",
+          type: "base64-transaction" as any, 
           transaction: serializedTx,
           message: "Donation successful ! You can now proceed to mint your NFT Supporter Badge. Please note, transaction fees will be covered by you to complete the minting process.",
           links: {
@@ -334,7 +334,7 @@ import {
      }).toString("base64");
 
        const responseBody: ActionPostResponse = {
-            type: "transaction",
+            type: "base64-transaction" as any, 
             transaction: serializedTxFee,
             message: "NFT Minted successfuly!",
             links: {
