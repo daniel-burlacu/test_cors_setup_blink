da9919ca19892d8b304d19f60a54905416e5b799
daniel-burlacu <daniel.burlacu1983@yahoo.se>
2025-07-01 16:40:23 +0200

    removing the any and base64 type

--- src/app/api/actions/route.ts
+++ src/app/api/actions/route.ts
@@ -216,9 +216,9 @@ import {
       verifySignatures: false,
      }).toString("base64");
 
-      const responseBody: ActionPostResponse = {
-          type: "base64-transaction", 
-          base64Transaction: serializedTx,
+           const responseBody: ActionPostResponse = {
+          type: "transaction",
+          transaction: serializedTx,
           message: "Donation successful ! You can now proceed to mint your NFT Supporter Badge. Please note, transaction fees will be covered by you to complete the minting process.",
           links: {
             next: {
@@ -242,7 +242,7 @@ import {
               },
             },
           },
-      } as any;
+      };
   
       // const responseBody: ActionPostResponse = await createPostResponse({
       //   fields: {
@@ -334,8 +334,8 @@ import {
      }).toString("base64");
 
        const responseBody: ActionPostResponse = {
-            type: "base64-transaction", 
-            base64Transaction: serializedTxFee,
+            type: "transaction",
+            transaction: serializedTxFee,
             message: "NFT Minted successfuly!",
             links: {
               next: {
@@ -359,7 +359,7 @@ import {
                 },
               },
             },
-        }as any;
+        };
   
         // const responseBody: ActionPostResponse = await createPostResponse({
         //   fields: {
